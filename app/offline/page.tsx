f20b6f650cc889e1d2c8a5f738ccf0b3a5d6b034
Ali Asadollahi <ali.asadolahi74@gmail.com>
2024-09-29 15:33:58 +0330

    Changed color

--- app/offline/page.tsx
+++ app/offline/page.tsx
@@ -9,7 +9,7 @@ const OfflinePage = () => {
                 <Link href="/offline2">Link</Link>
             </h1>
 
-            <button className="flex flex-col items-center bg-teal-800 rounded-full"></button>
+            <button className="flex flex-col items-center bg-teal-800 rounded-full">Click Here</button>
         </div>
 
     );
